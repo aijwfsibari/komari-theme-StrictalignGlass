742f56caad8f76af5d953497b1544977e12ad0a1
aijwfsibari <wen_zhenbin@qq.com>
2026-06-23 17:59:16 +0800

    name change

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -49,10 +49,10 @@ const formattedServerVersion = computed(() => serverVersion.value?.version ?? ''
           :content="`v${buildVersion}\n${buildGitHash}`"
         >
           <a
-            href="https://github.com/sanrokamlan-prog/komari-theme-Glassmorphism" target="_blank" rel="noopener noreferrer"
+            href="https://github.com/aijwfsibari/komari-theme-StrictalignGlass" target="_blank" rel="noopener noreferrer"
             class="transition-opacity hover:opacity-80"
           >
-            <span class="font-medium text-foreground">Komari Glassmorphism</span>
+            <span class="font-medium text-foreground">Komari AlignGlass</span>
           </a>
         </DataTooltip>
       </div>
